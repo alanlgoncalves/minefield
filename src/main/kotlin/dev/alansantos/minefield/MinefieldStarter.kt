0bbc681abda1c1dc8e39ab08b1144d11107b39fc
alanlgoncalves <alan.lgoncalves@outlook.com>
2020-04-10 11:27:36 -0300

    Changes on build.gradle to start the game

---
+++ src/main/kotlin/dev/alansantos/minefield/MinefieldStarter.kt
@@ -0,0 +1,7 @@
+package dev.alansantos.minefield
+
+import dev.alansantos.minefield.view.game.MinefieldWindow
+
+fun main(args : Array<String>) {
+    MinefieldWindow()
+}
--- src/main/kotlin/dev/alansantos/minefield/MonefieldStarter.kt
+++
@@ -1,7 +0,0 @@
-package dev.alansantos.minefield
-
-import dev.alansantos.minefield.view.menu.MinefieldMenu
-
-fun main(args : Array<String>) {
-    MinefieldMenu()
-}
